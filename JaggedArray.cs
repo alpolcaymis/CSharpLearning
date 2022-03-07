014d8fb87ca57224476497d5564eb3d54bbc9973
Alp Olcay Mis <71964088+alpolcaymis@users.noreply.github.com>
2022-03-07 18:09:40 +0300

    Jagged_Array_Print_on_Console

---
+++ JaggedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[][] jaggedArray = new int[][]
+            {
+                new int[] { 1, 2, 3, 4, 5, 6},
+                new int[] { 7, 8, 9},
+            };
+
+
+            foreach (int[] jagged in jaggedArray)
+            {
+                foreach (int i in jagged)
+                    Console.WriteLine(i);
+            }
+
+
+        }
+    }
+}
