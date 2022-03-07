4fe144cf44b34225a0212070fa845b3c4da14729
Alp Olcay Mis <71964088+alpolcaymis@users.noreply.github.com>
2022-03-07 22:53:40 +0300

    Find_array_in_array_from_its_name

---
+++ Array.IndexOf.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            string[] alp = new string[] { "Sevinç", "nigar" , "cosku", "seza"};
+            string[] habip = new string[] { "Habanne", "Hababa" };
+            string[] hakan = new string[] { "Hakanne", " Hakbaba" };
+
+            string[][] familyList = new string[][]
+            {
+                alp,
+                habip,
+                hakan
+                
+            };
+
+            for (int i = 0; i < alp.Length; i++)
+            {
+                Console.WriteLine(familyList[Array.IndexOf(familyList, alp)][i]);
+
+            }
+            
+
+            
+        }
+    }
+}
