2c9f59c6fa935521d22a6dc01f357e3ba7c60371
Alp Olcay Mis <71964088+alpolcaymis@users.noreply.github.com>
2022-03-09 12:54:34 +0300

    Find_Minimum_Value_Among_Array
    with using params keyword

---
+++ MinValueInArray.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int min = MinV2(5, 8, 2, 6, -44, -0093, 732423, 864, 31121, 8453, 20, 00001);
+
+            Console.WriteLine("The minimum is {0} : " , min);
+            
+             
+        }
+
+        public static int MinV2(params int[] numbers)
+        {
+            int min = int.MaxValue;
+
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                    min = number;
+
+            }
+            return min;
+        }
+
+
+        
+    }
+}
