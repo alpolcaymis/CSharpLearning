dbb023be4d5d45b96252c41e2a92bac1fefd8ee1
Alp Olcay Mis <71964088+alpolcaymis@users.noreply.github.com>
2022-03-25 17:15:14 +0300

    ListOfCurrentMatches
    ListBox Grid Binding

---
+++ ListBox.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_01C
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+      
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            List<Match> matches = new List<Match>();
+
+            matches.Add(new Match() { Team1 = "Bayern Munich", Team2 = "Real Madrid", Score1 = 3, Score2 = 2, Completion = 69 });
+            matches.Add(new Match() { Team1 = "Galatasaray", Team2 = "Fenerbahçe", Score1 = 1, Score2 = 0, Completion = 29 });
+            matches.Add(new Match() { Team1 = "PSG", Team2 = "Schalke04", Score1 = 3, Score2 = 5, Completion = 89 });
+            matches.Add(new Match() { Team1 = "FC Milan", Team2 = "Inter", Score1 = 0, Score2 = 1, Completion = 70 });
+            
+            lbMatches.ItemsSource = matches;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbMatches.SelectedItem != null)
+            {
+                MessageBox.Show("Selected Match :"
+                    + (lbMatches.SelectedItem as Match).Team1 + " "
+                    + (lbMatches.SelectedItem as Match).Score1 + " "
+                    + (lbMatches.SelectedItem as Match).Score2 + " "
+                    + (lbMatches.SelectedItem as Match).Team2 );
+
+            }
+
+        }
+    }
+
+    public class Match
+    {
+        public int Score1 { get; set; }
+        public int Score2 { get; set; }
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Completion { get; set; }
+    }
+}
